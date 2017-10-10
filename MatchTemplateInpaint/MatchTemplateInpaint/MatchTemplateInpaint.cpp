dd3a72c72cf12a3b9c1d1d8c562ba8ac7138c4aa
pushup666 <1149843835@qq.com>
2017-10-10 08:00:56 +0800

    Initial commit

---
+++ MatchTemplateInpaint/MatchTemplateInpaint/MatchTemplateInpaint.cpp
@@ -0,0 +1,112 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <opencv2/core/core.hpp> 
+#include <iostream>
+#include <stdio.h>
+
+#include <photo\photo.hpp>
+
+using namespace std;
+using namespace cv;
+
+Mat _img; Mat _result;Mat _inpaint;
+Mat _templ; Mat _mask;Mat _templ1; Mat _mask1;Mat _templ2; Mat _mask2;
+Point matchLoc;
+
+//CV_TM_SQDIFF        =0
+//CV_TM_SQDIFF_NORMED =1
+//CV_TM_CCORR         =2
+//CV_TM_CCORR_NORMED  =3
+//CV_TM_CCOEFF        =4
+//CV_TM_CCOEFF_NORMED =5
+int match_method = CV_TM_SQDIFF;
+
+void MatchingTemplate(Mat templ);
+void MatchingMask(Mat mask);
+
+int main()
+{
+	_img = imread("D:\\2.bmp");
+	_templ = imread("D:\\plus.bmp", 1);
+	_mask = imread("D:\\plusmask.bmp", 1);
+
+	_templ1 = imread("D:\\plus1.bmp", 1);
+	_mask1 = imread("D:\\plus1mask.bmp", 1);
+
+	_templ2 = imread("D:\\plus2.bmp", 1);
+	_mask2 = imread("D:\\plus2mask.bmp", 1);
+		
+	//cvtColor(_img, _img, CV_RGB2GRAY, 0);
+	//cvtColor(_templ, _templ, CV_RGB2GRAY, 0);
+	//cvtColor(_mask, _mask, CV_RGB2GRAY, 0);
+
+	imshow("1", _img);
+
+	MatchingTemplate(_templ1);
+	MatchingMask(_mask1);
+	imshow("2", _img);
+
+	MatchingTemplate(_templ2);
+	MatchingMask(_mask2);
+	imshow("3", _img);
+
+	MatchingTemplate(_templ);
+	MatchingMask(_mask);
+	imshow("4", _img);
+
+	MatchingTemplate(_templ);
+	MatchingMask(_mask);
+	imshow("5", _img);
+
+	Mat imageGray;
+	cvtColor(_img, imageGray, CV_RGB2GRAY, 0);
+	Mat imageMask = Mat(imageGray.size(), CV_8UC1, Scalar::all(0));
+	threshold(imageGray, imageMask, 250, 255, CV_THRESH_BINARY);
+	inpaint(_img, imageMask, _inpaint, 5, INPAINT_TELEA);
+
+	imshow("9", _inpaint);
+	cout << "Finish" <<endl;
+	waitKey();
+	return 0;
+}
+
+void MatchingTemplate(Mat templ)
+{
+	Mat img_display;
+	_img.copyTo(img_display);
+
+	int result_rows = _img.rows - templ.rows + 1;
+	int result_cols = _img.cols - templ.cols + 1;
+	
+	_result.create(result_cols, result_rows, CV_32FC1);
+
+	matchTemplate(_img, templ, _result, match_method);
+	normalize(_result, _result, 0, 1, NORM_MINMAX, -1, Mat());
+	
+	double minVal; double maxVal; Point minLoc; Point maxLoc;
+	minMaxLoc(_result, &minVal, &maxVal, &minLoc, &maxLoc, Mat());
+	matchLoc = (match_method == CV_TM_SQDIFF || match_method == CV_TM_SQDIFF_NORMED) ? minLoc : maxLoc;
+
+	//rectangle(img_display, matchLoc, Point(matchLoc.x + templ.cols , matchLoc.y + templ.rows), Scalar::all(0), 2, 8, 0);
+	//rectangle(_result,  Point(matchLoc.x - 0.5*templ.cols , matchLoc.y - 0.5*templ.rows), Point(matchLoc.x + 0.5*templ.cols , matchLoc.y + 0.5*templ.rows), Scalar::all(0), 2, 8, 0);
+
+	//imshow("image_window", img_display);
+	//imshow("result_window", _result);
+}
+
+void MatchingMask(Mat mask)
+{
+	for (auto i = 0; i < mask.rows; i++)
+	{
+		for (auto j = 0; j < mask.cols; j++)
+		{			
+			if (mask.at<Vec3b>(i,j)[0] == 255)
+			{
+				_img.at<Vec3b>(i+matchLoc.y,j+matchLoc.x)[0] = 255;
+				_img.at<Vec3b>(i+matchLoc.y,j+matchLoc.x)[1] = 255;
+				_img.at<Vec3b>(i+matchLoc.y,j+matchLoc.x)[2] = 255;
+			}
+		}
+	}
+}
+
---
+++ VideoWrite/VideoWrite/VideoWrite.cpp
@@ -0,0 +1,99 @@
+#include <iostream>                        // for standard I/O    cout
+#include <string>                          // for strings
+#include <ctime>
+
+#include <opencv2/core.hpp>                // Basic OpenCV structures (cv::Mat)
+#include <opencv2/videoio.hpp>             // Video write
+#include <opencv2/highgui/highgui.hpp>     // CV_FOURCC
+
+using namespace std;
+using namespace cv;
+
+int width = 400;
+int height = 400;
+
+int frames = 2000;
+RNG rng(static_cast<int>(time(nullptr)));
+
+static Mat NextFrame(Mat src)
+{
+	Mat res;
+	src.copyTo(res);
+		
+	for (auto i = 2; i < width - 2; i++)
+	{
+		for (auto j = 2; j < height - 2; j++)
+		{			
+			auto neighbor5 = 0;
+		
+			for (auto ii = -2; ii < 3; ii++)
+			{
+				for (auto jj = -2; jj < 3; jj++)
+				{
+					neighbor5 += src.at<uchar>(j+jj,i+ii);
+				}
+			}
+
+			neighbor5 -= src.at<uchar>(j,i);
+
+			if (rng.uniform(0,256) > neighbor5/24)
+			{
+				res.at<uchar>(j,i) = 0;
+			}
+			else
+			{
+				res.at<uchar>(j,i) = 255;
+			}
+		}
+	}
+
+	return res;
+}
+
+int main()
+{          
+	const string NAME = "D:\\TEST.avi";
+	auto S = Size(width, height);
+	VideoWriter outputVideo(NAME, CV_FOURCC('X','V','I','D'), 60, S, 0);
+
+    if (!outputVideo.isOpened())
+    {
+        cout  << "Could not open the output video for write: " << NAME << endl;
+        return -1;
+    }
+
+	Mat img(S,CV_8UC1,Scalar::all(255));
+
+	for (auto i = width/4; i < width/2; i++)
+	{
+		for (auto j = height/4; j < height/2; j++)
+		{			
+			img.at<uchar>(j,i) = 0;
+		}
+	}
+
+	for (auto i = width/2; i < width/4*3; i++)
+	{
+		for (auto j = height/2; j < height/4*3; j++)
+		{			
+			img.at<uchar>(j,i) = 0;
+		}
+	}
+
+	auto begin = static_cast<double>(getTickCount());
+
+	for (auto i = 0; i < frames; i++)
+	{
+		img = NextFrame(img);
+		outputVideo.write(img);
+	}
+	auto duration = static_cast<double>(getTickCount()) - begin;
+	duration /= getTickFrequency();
+
+	printf( "run time = %g s\n", duration);
+	printf( "fps = %g \n", frames / duration);
+    cout << "Finished writing" << endl;
+
+	system("pause");
+    return 0;
+}
