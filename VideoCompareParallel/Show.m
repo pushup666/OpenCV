7b086e0fafb64e16ee26b13bd32885cc36c333dc
pushup666 <1149843835@qq.com>
2017-11-10 16:33:27 +0800

    1

--- VideoCompareParallel/Show.m
+++ VideoCompareParallel/Show.m
@@ -1,10 +1,10 @@
 data1 = load('D:\Codecs\Beauty.libx264.5k.txt');
 psnr_data1 = data1(:,1);
 ssim_data1 = data1(:,2);
-data2 = load('D:\Codecs\Beauty.libx265.5k.txt');
+data2 = load('D:\Codecs\Beauty.libx265.5k.crf28.txt');
 psnr_data2 = data2(:,1);
 ssim_data2 = data2(:,2);
-data3 = load('D:\Codecs\Beauty.libx264.5k.txt');
+data3 = load('D:\Codecs\Beauty.libx265.5k.crf28.txt');
 psnr_data3 = data3(:,1);
 ssim_data3 = data3(:,2);
 
@@ -18,13 +18,13 @@ smooth_ssim_data3 = smooth(ssim_data3);
 figure(1);hold on;
 plot(smooth_psnr_data1,'r');
 plot(smooth_psnr_data2,'g');
-%plot(smooth_psnr_data3,'b');
+plot(smooth_psnr_data3,'b');
 title('psnr');
 
 figure(2);hold on;
 plot(smooth_ssim_data1,'r');
 plot(smooth_ssim_data2,'g');
-%plot(smooth_ssim_data3,'b');
+plot(smooth_ssim_data3,'b');
 title('ssim');
 
 mean_psnr_data1 = mean(psnr_data1);
